973e4fe14798db376841101b83c9a49ac6b687c1
Joshua <joshuagpatten@gmail.com>
2023-01-06 14:42:20 -0500

    Add comments/minor cleanup

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
+// Gameboard uses module pattern because only one is needed
 const gameboard = (function() {
   var turnCounter;
   var gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-  const render = () => {
-    turnCounter = 1;
-    gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-    for(var i = 0; i < 9; i++) {
-      let index = "" + i;
-      let ele = document.getElementById(index);
-      ele.innerText = gameboard[i];
-      ele.classList.remove("taken", "X", "O");
-      ele.setAttribute('onclick','caller(this)')
-    }
-  }
-  render();
 
-  (function() {
-
-    const button = document.getElementById('restartbutton');
-    button.addEventListener( 'click', render)
-  })();
-
-  const markSquare = (input) => {
+   //Input is passed in by caller function, here is where the square is actually "marked"
+   const markSquare = (input) => {
     id = parseInt(input.id);
     if(turnCounter % 2 ===0) {
       gameboard[id] = 'X';
@@ -42,7 +26,31 @@ const gameboard = (function() {
     }
     if(turnCounter === 10) alert("Tie game")
   }
+
+  //Used to display gameboard at beginning and when restart button is clicked
+  const render = () => {
+    turnCounter = 1;
+    gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+    for(var i = 0; i < 9; i++) {
+      let index = "" + i;
+      let ele = document.getElementById(index);
+      ele.innerText = gameboard[i];
+      ele.classList.remove("taken", "X", "O");
+      ele.setAttribute('onclick','caller(this)')
+    }
+  }
+  //Display game board when page loads
+  render();
+
+  //Add functionality to restart button
+  (function() {
+    const button = document.getElementById('restartbutton');
+    button.addEventListener( 'click', render)
+  })();
+
+ 
   
+  //Makes all squares unclickable after game ends 
   const disableClicks = () => {
     for(var i = 0; i < 9; i++) {
       let index = "" + i;
@@ -51,6 +59,7 @@ const gameboard = (function() {
     }
   }
 
+  //I'm sure there's a better way to do this, but brute force works for now
   const searchWin = () => {
     if(
       gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2] ||
@@ -73,27 +82,11 @@ const gameboard = (function() {
         alert("O wins")};
     }
   }
-
+  //The only thing that is exposed is markSquare, only did this because caller needs it
   return {markSquare};
 })();
 
-/* const player = (name) => {
-  return {name};
-}; */
-
+//Used to solve issue where markSquare is undefined and causes error when a square is clicked
 function caller(input) {
   gameboard.markSquare(input);
 }
-
-/* const createPlayers = ()=>{
-  let player1 = {
-    name: document.getElementById('player1').value
-  }
-  let player2 = {
-    name:document.getElementById('player2').value
-  }
-  document.forms[0].reset();
-  return false;
-} 
-
- */
